907641384c5b7f88c2279a19d226323b6808a56a
Alessandro <alessandropicchetti@hotmail.it>
2022-06-03 21:14:39 +0200

    lots of stuff

--- scripts/Game/Components/Damage/Modded/EXA_CharacterDamageManagerComponent.c
+++ scripts/Game/Components/Damage/Modded/EXA_CharacterDamageManagerComponent.c
@@ -1,15 +1,28 @@
 modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponent
 {
 
-	[Attribute("{9DEEB4290C38BF09}Particles/Enviroment/AddedBleeding.ptc", UIWidgets.Auto, desc: "Bleeding particle effect from body shots", category: "Bleeding", params: "ptc")]
-	private ResourceName m_sBleedingParticleMini;
+	[Attribute("{9DEEB4290C38BF09}Particles/Enviroment/BleedingBodyShots.ptc", UIWidgets.Auto, desc: "Bleeding particle effect from body shots", category: "Bleeding", params: "ptc")]
+	private ResourceName m_sBleedingParticleBody;
+	
+	[Attribute("{486BD20FE5257A35}Particles/Enviroment/BleedingHeadshots.ptc", UIWidgets.Auto, desc: "Bleeding particle effect from head shots", category: "Bleeding", params: "ptc")]
+	private ResourceName m_sBleedingParticleHeadshot;
+	
 
 	bool enableEffects;
+	bool enableBloodlustCompatibility;
 	
 	override void OnInit(IEntity owner)
 	{
+		enableEffects = true;
+		enableBloodlustCompatibility = true;
+		
+		
+		
+		if (enableBloodlustCompatibility)
+			InitBloodlust(owner);
+		
 
-		/* Settings initialization stuff */
+		/* Settings initialization stuff 
 		MCF_SettingsManager mcfSettingsManager = MCF_SettingsManager.GetInstance();
 		
 		const string fileNameJson = "EXA_Settings.json";
@@ -20,8 +33,24 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 
 		map<string, string> settings = mcfSettingsManager.Setup(MOD_ID, fileNameJson, mapDefaultValues, userFriendlyNames);
 		enableEffects = settings.Get("enableEffects").ToInt();
-
+*/
 	}
+	
+	
+	
+	
+	void InitBloodlust(IEntity owner)
+	{
+		auto world = owner.GetWorld();
+	
+		m_splatterBehavior = new BL_CharacterSplatterBehavior;
+		m_splatterBehavior.OnInit(owner, world, this);
+	
+		m_bleedBehavior = new BL_CharacterBleedBehavior;
+		m_bleedBehavior.OnInit(owner, world, this);
+	}
+	
+	
 
 //-----------------------------------------------------------------------------------------------------------
 	//! Called to start bleeding effect on a specificied bone
@@ -30,9 +59,7 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 		
 		if (enableEffects)
 		{
-			// Play Bleeding particle
-			if (m_sBleedingParticle.IsEmpty())
-				return;
+
 			
 			RemoveBleedingParticleEffect(hitZone);
 			
@@ -44,11 +71,19 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 			
 			
 			// TODO: Blood traces on ground that should be left regardless of clothing, perhaps just delayed
+			
+			// Headshots are considered as NOT COVERED!
 			SCR_CharacterHitZone characterHitZone = SCR_CharacterHitZone.Cast(hitZone);
+			
+			
+
 			if (characterHitZone.IsCovered())
-				chosenParticles = m_sBleedingParticleMini;
+				chosenParticles = m_sBleedingParticleBody;
 			else
-				chosenParticles = m_sBleedingParticle;
+			{
+				chosenParticles = m_sBleedingParticleHeadshot;
+
+			}
 			
 			
 			// Get bone node
@@ -58,7 +93,7 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 			if (!hitZone.TryGetColliderDescription(GetOwner(), colliderDescriptorIndex, transform, boneIndex, boneNode))
 				return;
 			
-			SCR_ParticleEmitter particleEmitter = SCR_ParticleAPI.PlayOnObjectPTC(GetOwner(), m_sBleedingParticle, vector.Zero, vector.Zero, boneNode);
+			SCR_ParticleEmitter particleEmitter = SCR_ParticleAPI.PlayOnObjectPTC(GetOwner(), chosenParticles, vector.Zero, vector.Zero, boneNode);
 			SCR_ParticleAPI.LerpAllEmitters(particleEmitter, bleedingRate * m_fBleedingParticleRateScale, EmitterParam.BIRTH_RATE);
 			
 			if (!m_mBleedingParticles)
